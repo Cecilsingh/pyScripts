acf774be17ceca5b97368b837ea89285afaf0748
Cecil Singh <54638818+Cecilsingh@users.noreply.github.com>
2024-09-04 14:51:32 +1000

    Update tmpRestore.py

--- files/tmpRestore.py
+++ files/tmpRestore.py
@@ -1,1 +1,11 @@
+#Import shutil package for file operations
+import shutil
 
+#Specify backup file
+source_recovery_folder = '/home/$USER/backup.txt'
+
+#Specify destination file to restore.
+backup_recovery_folder = '/home/$USER/initial.txt'
+
+#Use the shutil package to copy the source folder 
+shutil.copytree(backup_recovery_folder, source_recovery_folder)
